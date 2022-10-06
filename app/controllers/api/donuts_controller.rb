fd2ed26a7694006d97c26cbea7e7772e2e4bfdce
Alex Racinski <aracinski2@gmail.com>
2022-10-05 19:23:48 -0500

    create donuts show action

--- app/controllers/api/donuts_controller.rb
+++ app/controllers/api/donuts_controller.rb
@@ -5,7 +5,7 @@ class Api::DonutsController < ApplicationController
   end
 
   def show
-    @donut = Donut.find_by(id: 1)
+    @donut = Donut.find_by(id: params[:id])
     render 'show.json.jb'
   end
   
